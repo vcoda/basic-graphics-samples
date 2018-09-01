b741707ed0958188fc31c4cf75e7f845ecc3d2b0
Victor Coda <victor.coda@gmail.com>
2018-09-01 14:38:08 +0300

    Added check for valid pointer

--- framework/linearAllocator.cpp
+++ framework/linearAllocator.cpp
@@ -15,35 +15,41 @@ LinearAllocator::~LinearAllocator()
 
 void *LinearAllocator::alloc(size_t size)
 {
-    void *p;
-    if (size + bytesAllocated <= bufferSize)
-    {   // Simply eat new chunk from fixed buffer until overflow
-        p = head;
-        head = (char *)MAGMA_ALIGN((intptr_t)(head + size));
+    void *p = nullptr;
+    if (size)
+    {
+        if (size + bytesAllocated <= bufferSize)
+        {   // Simply eat new chunk from fixed buffer until overflow
+            p = head;
+            head = (char *)MAGMA_ALIGN((intptr_t)(head + size));
+        }
+        else
+        {   // Use malloc() on overflow
+            p = MAGMA_MALLOC(size);
+        }
+        MAGMA_ASSERT(MAGMA_ALIGNED(p));
+        bytesAllocated += size;
+        blocks[p] = size;
     }
-    else
-    {   // Use malloc() on overflow
-        p = MAGMA_MALLOC(size);
-    }
-    MAGMA_ASSERT(MAGMA_ALIGNED(p));
-    bytesAllocated += size;
-    blocks[p] = size;
     return p;
 }
 
 void LinearAllocator::free(void *p)
 {
-    auto it = blocks.find(p);
-    if (it != blocks.end())
+    if (p)
     {
-        bytesAllocated -= it->second;
-        blocks.erase(it);
+        auto it = blocks.find(p);
+        if (it != blocks.end())
+        {
+            bytesAllocated -= it->second;
+            blocks.erase(it);
+        }
+        if (p < buffer || p > head)
+            ::MAGMA_FREE(p);
+        else
+            // Do nothing
+            ;
     }
-    if (p < buffer || p > head)
-        ::MAGMA_FREE(p);
-    else
-        // Do nothing
-        ;
 }
 
 size_t LinearAllocator::getBytesAllocated() const
