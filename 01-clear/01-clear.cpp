1df0e37929956d2ddb8058b1be60e5e0b2525c96
Victor Coda <victor.coda@gmail.com>
2018-10-14 20:25:57 +0300

    Removed padding

--- 01-clear/01-clear.cpp
+++ 01-clear/01-clear.cpp
@@ -37,10 +37,6 @@ public:
             renderFinished,
             waitFences[bufferIndex]);
     }
-
-#if defined(VK_USE_PLATFORM_XLIB_KHR) || defined(VK_USE_PLATFORM_XCB_KHR)
-    char padding[16];
-#endif
 };
 
 std::unique_ptr<IApplication> appFactory(const AppEntry& entry)
