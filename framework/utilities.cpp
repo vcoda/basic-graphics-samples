88ddc3427b607a3470dce24bd6b458e37f32ad0a
Victor Coda <victor.coda@gmail.com>
2025-01-09 14:07:30 +0200

    Comment out unused parameters

--- framework/utilities.cpp
+++ framework/utilities.cpp
@@ -81,9 +81,9 @@ uint32_t getSupportedMultisampleLevel(std::shared_ptr<magma::PhysicalDevice> phy
     return 1;
 }
 
-VkBool32 VKAPI_PTR reportCallback(VkDebugReportFlagsEXT flags, VkDebugReportObjectTypeEXT objectType,
-    uint64_t object, size_t location, int32_t messageCode,
-    const char *pLayerPrefix, const char *pMessage, void *pUserData)
+VkBool32 VKAPI_PTR reportCallback(VkDebugReportFlagsEXT flags, VkDebugReportObjectTypeEXT /* objectType */,
+    uint64_t /* object */, size_t location, int32_t messageCode,
+    const char *pLayerPrefix, const char *pMessage, void */* pUserData */)
 {
     if (strstr(pMessage, "Extension"))
         return VK_FALSE;
